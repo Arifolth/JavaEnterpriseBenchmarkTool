7071d82e00b5aa3177f78bf34a62c5b14e57686c
ANilov <ANilov@yotateam.com>
2017-02-26 17:32:08 +0300

    mirror output to logfile

--- Launcher/bin/benchmarktool.sh
+++ Launcher/bin/benchmarktool.sh
@@ -2,4 +2,4 @@
 
 #bash must be available on target system    
 
-java -jar launcher-1.0-SNAPSHOT.jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 ru.arifolth.benchmark.BenchmarkMain
+java -jar -Dlogback.configurationFile=./logback.xml launcher-1.0-SNAPSHOT.jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 ru.arifolth.benchmark.BenchmarkMain
