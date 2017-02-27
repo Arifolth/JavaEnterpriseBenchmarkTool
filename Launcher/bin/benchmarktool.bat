7b1d0f8baedae8f59c94f4245dd21a11445e655b
ANilov <ANilov@yotateam.com>
2017-02-28 00:52:41 +0300

    Max Heap size increased

--- Launcher/bin/benchmarktool.bat
+++ Launcher/bin/benchmarktool.bat
@@ -5,7 +5,7 @@ setlocal
 for /f "tokens=*" %%a in ('dir /b *.jar') do call :concat %%a
 
 echo clp: %clp%
-java -version:1.6 -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms512m -Xmx1g -Dlogback.configurationFile=./logback.xml -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 launcher-1.0-SNAPSHOT.jar -cp %clp% ^
+java -version:1.6 -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms512m -Xmx1280m -Dlogback.configurationFile=./logback.xml -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 launcher-1.0-SNAPSHOT.jar -cp %clp% ^
 ru.arifolth.benchmark.BenchmarkMain
 goto :eof
 
--- Launcher/bin/benchmarktool.sh
+++ Launcher/bin/benchmarktool.sh
@@ -2,4 +2,4 @@
 
 #bash must be available on target system    
 
-java -version:1.6 -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms512m -Xmx1g -Dlogback.configurationFile=./logback.xml -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 launcher-1.0-SNAPSHOT.jar ru.arifolth.benchmark.BenchmarkMain
+java -version:1.6 -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms512m -Xmx1280m -Dlogback.configurationFile=./logback.xml -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 launcher-1.0-SNAPSHOT.jar ru.arifolth.benchmark.BenchmarkMain
